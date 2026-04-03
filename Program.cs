6b7f27c0a36940ae5b14b3caceae960eff8ada1b
Mri-cia <rawikara8c@gmail.com>
2026-04-03 20:14:50 +0700

    menambahkan class SayaMusicTrack

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine($"Input Song");
+Console.WriteLine("------------------");
+Console.Write("Title: ");
+string? judul = Console.ReadLine();
+
+Console.Write("Play Count: ");
+
+if (!int.TryParse(Console.ReadLine(), out int playCount))
+{
+    Console.WriteLine("Invalid input for play count. Please enter a valid integer.");
+}
+
+Console.WriteLine("------------------");
+
+Console.WriteLine();
+
+var song = new SayaMusicTrack(judul ?? "Unknown Title");
+song.IncreasePlayCount(playCount);
+
+Console.WriteLine("Printed Song");
+Console.WriteLine("------------------");
+song.PrintTrackDetails();
+    
---
+++ SayaMusicTrack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class SayaMusicTrack
+{
+    private int id;
+    private string playCount;
+    private string title;
+
+    public SayaMusicTrack(string title) 
+    {
+        id = Random.Shared.Next(1, 99999);
+        playCount = "0";
+        this.title = title;
+    }
+
+    public void IncreasePlayCount(int count)
+    { 
+        playCount = (int.Parse(playCount) + count).ToString();
+    }
+
+    public void PrintTrackDetails()
+    {
+        Console.WriteLine($"""
+            Title: {title} 
+            ID: {id} 
+            Playcount: {playCount}
+            """);
+    }
+}
